891cddea7c14fee9f2e766748046f28c377c4e22
Paul Gilzow <paul.gilzow@platform.sh>
2024-11-06 15:23:09 -0600

    init commit

---
+++ ms-dbu-command.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WP_CLI\HelloWorld;
+
+use WP_CLI;
+
+if ( ! class_exists( '\WP_CLI' ) ) {
+	return;
+}
+
+$wpcli_ms_dbu_autoloader = __DIR__ . '/vendor/autoload.php';
+
+if ( file_exists( $wpcli_ms_dbu_autoloader ) ) {
+  require_once $wpcli_ms_dbu_autoloader;
+}
+
+WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
+  'before_invoke' => static function() {
+    if ( !is_multisite()) {
+      WP_CLI::error('Not a multisite');
+    }
+
+    if(!getenv('PLATFORM_APPLICATION_NAME')) {
+      WP_CLI::error('Required environment variables are missing. Are you sure you\'re on Platform.sh?');
+    }
+  }
+] );
---
+++ src/MsDbuCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace WP_CLI\MsDbu;
+
+use WP_CLI;
+use WP_CLI_Command;
+
+class MsDbuCommand extends WP_CLI_Command {
+
+  /**
+   * Updates WordPress multisites in non-production environments on Platform.sh.
+   *
+   * ## OPTIONS
+   *  [--routes=<routes>]
+   * : JSON object that describes the routes for the environment. @see PLATFORM_ROUTES https://docs.platform.sh/development/variables/use-variables.html#use-provided-variables
+   *
+   * [--app-name=<app-name>]
+   * : The app name as set in your app configuration.
+   *
+   * ## EXAMPLES
+   *
+   *     # Update the database with environment routes
+   *     $ wp ms-dbu
+   *     Success: All domains have been updated!
+   *
+   * @when before_wp_load
+   *
+   * @param array $args       Indexed array of positional arguments.
+   * @param array $assoc_args Associative array of associative arguments.
+   */
+
+  protected array $rawRoutes = [];
+
+  protected array $filteredRoutes = [];
+  protected string $appName;
+  protected string $envVarPrefix = "PLATFORM_";
+  protected array $searchColumns = [
+    'option_value',
+    'post_content',
+    'post_excerpt',
+    'post_content_filtered',
+    'meta_value',
+    'domain'
+  ];
+
+  public function __invoke( $args, $assoc_args ) {
+    //figure out where we get our route info
+    $routes = (isset($assoc_args['app-name']) && "" !== $assoc_args['app-name']) ?: $this->getRouteFromEnvVar();
+    //save our raw routes data
+    $this->setRawRoutes($this->parseRouteJson($routes));
+    //save our app name
+    $this->setAppName((isset($assoc_args['app-name']) && "" !== $assoc_args['app-name']) ?: $this->getEnvVar('NAME'));
+    //get our filtered route data
+    $this->getFilteredRoutes();
+
+    WP_CLI::log("Filtered Routes");
+    WP_CLI::log(var_export($this->filteredRoutes,true));
+
+  }
+
+  protected function parseRouteJson(string $routeInfo) {
+    $routes = [];
+    if(!json_validate($routeInfo)) {
+      WP_CLI::error('Route information does not appear to be valid JSON. Exiting.');
+    }
+
+    try {
+      $routes = json_decode($routeInfo, true, 512, JSON_THROW_ON_ERROR);
+    } catch (\JsonException $e) {
+      WP_CLI::error(printf('Unable to parse route information. Is it valid JSON? %s', $e->getMessage()));
+    }
+
+    return $routes;
+  }
+
+  protected function getFilteredRoutes(): void {
+    $appName = $this->appName;
+    $this->filteredRoutes = array_filter($this->rawRoutes, static function ($route) use ($appName) {
+      return (isset($route['upstream']) && $appName === $route['upstream']);
+    });
+  }
+
+  protected function getRouteFromEnvVar(): string {
+    return base64_decode($this->getEnvVar('ROUTES'));
+  }
+
+  protected function setRawRoutes(array $routes): void {
+    $this->rawRoutes = $routes;
+  }
+
+  protected function setAppName(string $name) {
+    $this->appName = $name;
+  }
+
+  protected function getEnvVar(string $varName) {
+    $envVarToGet = $this->envVarPrefix.$varName;
+    if(!getenv($envVarToGet) || "" === getenv($envVarToGet)) {
+      WP_CLI::error(printf("%s is not set or empty. Are you sure you're running on Platform.sh?", $envVarToGet));
+    }
+    return getenv($envVarToGet);
+  }
+}
