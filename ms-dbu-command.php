b3fe56eabaf938097bdf1e49cb1d40a0cb096cf9
Paul Gilzow <paul.gilzow@platform.sh>
2024-11-15 15:40:07 -0600

    removes duplicated code and replaces with static methods from MsDbuCommand class

--- ms-dbu-command.php
+++ ms-dbu-command.php
@@ -3,6 +3,7 @@
 namespace WP_CLI\MsDbu;
 
 use WP_CLI;
+use WP_CLI\MsDbu\MsDbuCommand;
 
 if ( ! class_exists( '\WP_CLI' ) ) {
 	return;
@@ -26,35 +27,20 @@ WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
   }
 ] );
 
-WP_CLI::add_hook('after_wp_config_load', static function (){
+WP_CLI::add_hook('after_wp_config_load', static function () {
   if(!getenv('PLATFORM_APPLICATION_NAME') || !getenv('PLATFORM_ROUTES')) {
     WP_CLI::debug("PLATFORM_APPLICATION_NAME and/or PLATFORM_ROUTES are missing. Unable to determine default url.");
     return;
   }
-  /**
-   * @todo much of this is also done in the MsDbuCommand class. Could we potentially set those methods as static so we
-   * can use them here as well?
-   */
 
-  $rawRoutes = base64_decode(getenv('PLATFORM_ROUTES'));
+  $rawRoutes = \WP_CLI\MsDbu\MsDbuCommand::getRouteFromEnvVar();
+  $appName = \WP_CLI\MsDbu\MsDbuCommand::getEnvVar('APPLICATION_NAME');
+  $filteredRoutes = \WP_CLI\MsDbu\MsDbuCommand::getFilteredRoutes($rawRoutes,$appName);
+  $defaultRouteInfo = \WP_CLI\MsDbu\MsDbuCommand::retrieveDefaultDomainInfo($filteredRoutes);
 
-  try {
-    $routes = \json_decode($rawRoutes, true, 512, JSON_THROW_ON_ERROR);
-  } catch (\JsonException $e) {
-    WP_CLI::error(\sprintf('Unable to parse route information. Is it valid JSON? %s', $e->getMessage()));
-  }
 
-  $appName = getenv('PLATFORM_APPLICATION_NAME');
-  //get all the valid routes that map to this app
-  $aryUpstreamRoutes = array_filter($routes, static function ($route) use ($appName) {
-    return isset($route['upstream']) && $appName === $route['upstream'] && $route['primary'];
-  });
-
-//  $site_host = parse_url(array_key_first(array_filter($aryUpstreamRoutes, static function ($route) {
-//    return $route['primary'];
-//  })),PHP_URL_HOST);
   //grab the first item
-  $mainRoute = reset($aryUpstreamRoutes);
+  $mainRoute = reset($defaultRouteInfo);
   WP_CLI::log(sprintf('Setting the WPCLI to use %s as the url for this command',$mainRoute['production_url']));
   WP_CLI::set_url($mainRoute['production_url']);
 
