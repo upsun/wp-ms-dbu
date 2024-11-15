4a58cf02a693628a1aa49db3814b411a6524cc2d
Paul Gilzow <paul.gilzow@platform.sh>
2024-11-15 14:49:21 -0600

    nope.

--- ms-dbu-command.php
+++ ms-dbu-command.php
@@ -16,8 +16,6 @@ if ( file_exists( $wpcli_ms_dbu_autoloader ) ) {
 
 WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
   'before_invoke' => static function() {
-    WP_CLI::log('Hello from before invoke');
-
     if ( !is_multisite()) {
       WP_CLI::error('Not a multisite');
     }
@@ -25,71 +23,39 @@ WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
     if(!getenv('PLATFORM_APPLICATION_NAME')) {
       WP_CLI::error('Required environment variables are missing. Are you sure you\'re on Platform.sh?');
     }
+  }
+] );
 
-    if(!getenv('PLATFORM_ROUTES')) {
-      WP_CLI::debug("PLATFORM_ROUTES is not available so skipping automated url set.");
-      return;
-    }
+WP_CLI::add_hook('after_wp_config_load', static function (){
+  if(!getenv('PLATFORM_APPLICATION_NAME') || !getenv('PLATFORM_ROUTES')) {
+    WP_CLI::debug("PLATFORM_APPLICATION_NAME and/or PLATFORM_ROUTES are missing. Unable to determine default url.");
+    return;
+  }
+  /**
+   * @todo much of this is also done in the MsDbuCommand class. Could we potentially set those methods as static so we
+   * can use them here as well?
+   */
 
-    /**
-     * @todo much of this is also done in the MsDbuCommand class. Could we potentially set those methods as static so we
-     * can use them here as well?
-     */
+  $rawRoutes = base64_decode(getenv('PLATFORM_ROUTES'));
 
-    $rawRoutes = base64_decode(getenv('PLATFORM_ROUTES'));
+  try {
+    $routes = \json_decode($rawRoutes, true, 512, JSON_THROW_ON_ERROR);
+  } catch (\JsonException $e) {
+    WP_CLI::error(\sprintf('Unable to parse route information. Is it valid JSON? %s', $e->getMessage()));
+  }
 
-    try {
-      $routes = \json_decode($rawRoutes, true, 512, JSON_THROW_ON_ERROR);
-    } catch (\JsonException $e) {
-      WP_CLI::error(\sprintf('Unable to parse route information. Is it valid JSON? %s', $e->getMessage()));
-    }
-
-    $appName = getenv('PLATFORM_APPLICATION_NAME');
-    //get all the valid routes that map to this app
-    $aryUpstreamRoutes = array_filter($routes, static function ($route) use ($appName) {
-      return isset($route['upstream']) && $appName === $route['upstream'] && $route['primary'];
-    });
+  $appName = getenv('PLATFORM_APPLICATION_NAME');
+  //get all the valid routes that map to this app
+  $aryUpstreamRoutes = array_filter($routes, static function ($route) use ($appName) {
+    return isset($route['upstream']) && $appName === $route['upstream'] && $route['primary'];
+  });
 
 //  $site_host = parse_url(array_key_first(array_filter($aryUpstreamRoutes, static function ($route) {
 //    return $route['primary'];
 //  })),PHP_URL_HOST);
-    //grab the first item
-    $mainRoute = reset($aryUpstreamRoutes);
-    WP_CLI::log(sprintf('Setting the WPCLI to use %s as the url for this command',$mainRoute['production_url']));
-    WP_CLI::set_url($mainRoute['production_url']);
+  //grab the first item
+  $mainRoute = reset($aryUpstreamRoutes);
+  WP_CLI::log(sprintf('Setting the WPCLI to use %s as the url for this command',$mainRoute['production_url']));
+  WP_CLI::set_url($mainRoute['production_url']);
 
-  }
-] );
-
-//WP_CLI::add_hook('after_wp_config_load', static function (){
-//  if(!getenv('PLATFORM_APPLICATION_NAME') || !getenv('PLATFORM_ROUTES')) {
-//    return;
-//  }
-//  /**
-//   * @todo much of this is also done in the MsDbuCommand class. Could we potentially set those methods as static so we
-//   * can use them here as well?
-//   */
-//
-//  $rawRoutes = base64_decode(getenv('PLATFORM_ROUTES'));
-//
-//  try {
-//    $routes = \json_decode($rawRoutes, true, 512, JSON_THROW_ON_ERROR);
-//  } catch (\JsonException $e) {
-//    WP_CLI::error(\sprintf('Unable to parse route information. Is it valid JSON? %s', $e->getMessage()));
-//  }
-//
-//  $appName = getenv('PLATFORM_APPLICATION_NAME');
-//  //get all the valid routes that map to this app
-//  $aryUpstreamRoutes = array_filter($routes, static function ($route) use ($appName) {
-//    return isset($route['upstream']) && $appName === $route['upstream'] && $route['primary'];
-//  });
-//
-////  $site_host = parse_url(array_key_first(array_filter($aryUpstreamRoutes, static function ($route) {
-////    return $route['primary'];
-////  })),PHP_URL_HOST);
-//  //grab the first item
-//  $mainRoute = reset($aryUpstreamRoutes);
-//  WP_CLI::log(sprintf('Setting the WPCLI to use %s as the url for this command',$mainRoute['production_url']));
-//  WP_CLI::set_url($mainRoute['production_url']);
-//
-//});
+});
