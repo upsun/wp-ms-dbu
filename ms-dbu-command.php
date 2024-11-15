94480969354d4a8634450534780b9c30944f18ff
Paul Gilzow <paul.gilzow@platform.sh>
2024-11-15 14:13:29 -0600

    getting closer

--- ms-dbu-command.php
+++ ms-dbu-command.php
@@ -28,9 +28,26 @@ WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
   }
 ] );
 
-WP_CLI::add_hook('after_wp_load', static function (){
-  $sites = get_sites();
-  WP_CLI::log('sites');
-  WP_CLI::log(var_export($sites,true));
+WP_CLI::add_hook('after_wp_config_load', static function (){
+  $rawRoutes = base64_decode(getenv('PLATFORM_ROUTES'));
+
+  try {
+    $routes = \json_decode($rawRoutes, true, 512, JSON_THROW_ON_ERROR);
+  } catch (\JsonException $e) {
+    WP_CLI::error(\sprintf('Unable to parse route information. Is it valid JSON? %s', $e->getMessage()));
+  }
+
+  $appName = getenv('PLATFORM_APPLICATION_NAME');
+  //get all the valid routes that map to this app
+  $aryUpstreamRoutes = array_filter($routes, static function ($route) use ($appName) {
+    return isset($route['upstream']) && $appName === $route['upstream'];
+  });
+
+//  $site_host = parse_url(array_key_first(array_filter($aryUpstreamRoutes, static function ($route) {
+//    return $route['primary'];
+//  })),PHP_URL_HOST);
+
+  WP_CLI::log('Upstream Routes');
+  WP_CLI::log(var_export($aryUpstreamRoutes,true));
 
 });
