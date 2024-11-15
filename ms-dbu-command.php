d40f02a92d1a3c97fc220dd745c2046152bd0d25
Paul Gilzow <paul.gilzow@platform.sh>
2024-11-15 16:11:49 -0600

    adds todos

--- ms-dbu-command.php
+++ ms-dbu-command.php
@@ -28,19 +28,24 @@ WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
 ] );
 
 WP_CLI::add_hook('after_wp_config_load', static function () {
+  /**
+   * @todo these can be replaced with calls to \WP_CLI\MsDbu\MsDbuCommand::getEnvVar()
+   */
   if(!getenv('PLATFORM_APPLICATION_NAME') || !getenv('PLATFORM_ROUTES')) {
     WP_CLI::debug("PLATFORM_APPLICATION_NAME and/or PLATFORM_ROUTES are missing. Unable to determine default url.");
     return;
   }
 
-
+  /**
+   * @todod Should we create a function that'll bundle all this together so we can have one call?
+   */
   $rawRoutes = \WP_CLI\MsDbu\MsDbuCommand::parseRouteJson(\WP_CLI\MsDbu\MsDbuCommand::getRouteFromEnvVar());
   $appName = \WP_CLI\MsDbu\MsDbuCommand::getEnvVar('APPLICATION_NAME');
   $filteredRoutes = \WP_CLI\MsDbu\MsDbuCommand::getFilteredRoutes($rawRoutes,$appName);
   $defaultRouteInfo = \WP_CLI\MsDbu\MsDbuCommand::retrieveDefaultDomainInfo($filteredRoutes);
 
 
-  //grab the first item
+  //@todo we need to verify there is one and only one item in the array
   $mainRoute = reset($defaultRouteInfo);
   WP_CLI::log(sprintf('Setting the WPCLI to use %s as the url for this command',$mainRoute['production_url']));
   WP_CLI::set_url($mainRoute['production_url']);
