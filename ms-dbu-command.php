3643d67ed8a70c02adb848ff44c822f09408962d
Paul Gilzow <paul.gilzow@platform.sh>
2024-11-15 09:59:16 -0600

    another experiment

--- ms-dbu-command.php
+++ ms-dbu-command.php
@@ -27,3 +27,7 @@ WP_CLI::add_command( 'ms-dbu', MsDbuCommand::class, [
     }
   }
 ] );
+
+WP_CLI::add_hook('after_wp_config_load', static function (){
+  WP_CLI::log('hello from after config load');
+});
